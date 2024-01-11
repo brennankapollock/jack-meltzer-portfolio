e9cd53536ab6f3130f32142d6d243f945d543647
brennankapollock <brennankapollock@gmail.com>
2024-01-10 18:24:42 -0800

    next config updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  basePath: "/jack-meltzer-portfolio", // Replace with your GitHub repository name
+};
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+  // other configurations...
+};
